45605dad7ca927e4f5be5e152eeb232abedee4bd
hakim njim <37658409+RymHamdi@users.noreply.github.com>
2023-10-09 10:09:54 +0100

    All drone prefabs

--- D_Simulation/Assets/Drone_Mvp1/nevigator.cs
+++ D_Simulation/Assets/Drone_Mvp1/nevigator.cs
@@ -10,7 +10,7 @@ public class nevigator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        drone = FindObjectOfType<Drone_Engine>().gameObject.transform;
+      //  drone = FindObjectOfType<Drone_Engine>().gameObject.transform;
     }
 
     // Update is called once per frame
